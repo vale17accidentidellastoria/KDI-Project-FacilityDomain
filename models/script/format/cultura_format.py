5267a1c7e4966ae09a6005cb04dda78960e2973b
Seyyed Arya Hassanli <31996976+AryaHassanli@users.noreply.github.com>
2019-12-10 18:50:09 +0100

    Cultura edited

---
+++ models/script/format/cultura_format.py
@@ -0,0 +1,6 @@
+import json
+import requests
+
+
+def rm_main(JSONString):
+	return JSONString
--- models/script/structure/cultura.py
+++ models/script/structure/cultura.py
@@ -1,81 +1,81 @@
 import json
-import requests
+import re
+
 import pandas as pd
+import requests
 
-
-class eventObj:
-
-	def __init__(self,
-	             title="",
-	             ScienceEvent=False,
-	             VisualArtsEvent=False,
-	             MusicEvent=False,
-	             ScreeningEvent=False,
-	             TheatreEvent=False,
-	             TalkEvent=False,
-	             GeneralEvent=False,
-	             date="",
-	             time="",
-	             locationName="",
-	             locationURL="",
-	             suitableFor="",
-	             source="",
-	             description="",
-	             other="",
-	             contact="",
-	             cost="",
-	             link=""):
-		self.source = source.replace("\n", " ,")
-		self.ScienceEvent = ScienceEvent
-		self.VisualArtsEvent = VisualArtsEvent
-		self.MusicEvent = MusicEvent
-		self.ScreeningEvent = ScreeningEvent
-		self.TheatreEvent = TheatreEvent
-		self.TalkEvent = TalkEvent
-		self.GeneralEvent = GeneralEvent
-		self.suitableFor = suitableFor.replace("\n", " ,")
-		self.title = title.replace("\n", " ,")
-		self.date = date.replace("\n", " ,")
-		self.time = time.replace("\n", " ,")
-		self.locationName = locationName.replace("\n", " ,")
-		self.locationURL = locationURL.replace("\n", " ,")
-		self.description = description.replace("\n", " ,")
-		self.contact = contact.replace("\n", " ,")
-		self.cost = cost.replace("\n", " ,")
-		self.other = other.replace("\n", " ,")
-		self.link = link.replace("\n", " ,")
+classes_txt = requests.get('https://raw.githubusercontent.com/andreamatt/KDI/master/scripts/structure/classes.py').text
+exec(classes_txt)
+constants_txt = requests.get('https://raw.githubusercontent.com/andreamatt/KDI/master/scripts/constants.py').text
+exec(constants_txt)
 
 
 def rm_main(JSONString):
-	with open('C:/Users/andre/Desktop/kdi/scraping/KDI/DBG/structure.json', 'w') as outfile:
-		json.dump(json.loads(JSONString), outfile, indent="\t")
-
+	dictionary = {
+		"Cultural exhibitions and events,"            : "GeneralEvent",
+		"Cultural exhibitions and events,Guided tour,": "GeneralEvent",
+		"Dance,Opera and modern ballet,"              : "TheatreEvent",
+		"Drama,"                                      : "TheatreEvent",
+		"Meetings and conferences,"                   : "TalkEvent",
+		"Meetings and conferences,Workshop,"          : "ScienceEvent",
+		"Music,"                                      : "MusicEvent",
+		"Music,Classical music concert,"              : "MusicEvent",
+		"Music,Jazz concert,"                         : "MusicEvent",
+		"exhibition,"                                 : "GeneralEvent",
+		"exhibition,Art exhibition,"                  : "VisualArtsEvent",
+		"exhibition,Photographic exhibition,"         : "VisualArtsEvent",
+		
+	}
 	cultura = json.loads(JSONString)
-	events = []
+	events = {}
 	for day in cultura['result']['events']:
 		for eventType in day['tipo_evento']:
-			for event in eventType['events']:
+			for e in eventType['events']:
 				subSubCategory = ""
-				for subType in event['tipo_evento']:
+				for subType in e['tipo_evento']:
 					if subType['name'] is not None:
 						subSubCategory += subType['name'] + ","
-				if len(event['luogo_della_cultura']) == 0:
-					event['luogo_della_cultura'].append({"name": ""})
-				events.append(
-				    eventObj(
-				        source="cultura",
-				        category="Cultural",
-				        subCategory=eventType['name'],
-				        subSubCategory=subSubCategory,
-				        title=event['name'],
-				        link=event['href'],
-				        date=day['day']['identifier'],
-				        time=event['orario_svolgimento'],
-				        locationName=event['comune'][0]['name'] + " --SELF-- " + event['luogo_svolgimento'] + " --SELF-- " +
-				        event['luogo_della_cultura'][0]['name'],
-				        cost=event['costi'],
-				        contact=event['contact'],
-				        description=event['description']))
-	events = [ob.__dict__ for ob in events]
-	df = pd.DataFrame(events)
-	return df
+				if len(e['luogo_della_cultura']) == 0:
+					e['luogo_della_cultura'].append({"name": ""})
+				subSubCategory = dictionary[subSubCategory]
+				location = e['comune'][0]['name'] + ", " + e['luogo_svolgimento'] + ", " + e['luogo_della_cultura'][0][
+					'name']
+				startDate = day['day']['identifier']
+				endDate = day['day']['identifier']
+				startTime = ""
+				endTime = ""
+				time = e['orario_svolgimento']
+				tmp = re.findall(r'at \d+\.\d+', time)
+				if len(tmp):
+					startTime = tmp[0]
+				else:
+					tmp = re.findall(r'\d+\.\d+', time)
+					if len(tmp) == 0:
+						tmp = re.findall(r'\d+\,\d+', time)
+						if len(tmp) == 0:
+							pass
+						elif len(tmp) == 1:
+							startTime = tmp[0].replace(',', ':')
+						elif len(tmp) > 1:
+							startTime = tmp[0].replace(',', ':')
+							endTime = tmp[1].replace(',', ':')
+						pass
+					elif len(tmp) == 1:
+						startTime = tmp[0].replace('.', ':')
+						pass
+					elif len(tmp) > 1:
+						startTime = tmp[0].replace('.', ':')
+						endTime = tmp[1].replace('.', ':')
+						
+				gen = GeneralEvent(e['name'], e['costi'], e['description'], e['href'], '', '', '', location)
+				time = Time(startDate, endDate, startTime, endTime)
+				
+				event = {}
+				for k, v in gen.items():
+					event[f'GEN_{k}'] = v
+				for k, v in time.items():
+					event[f'TIME_{k}'] = v
+				if subSubCategory not in events:
+					events[subSubCategory] = []
+				events[subSubCategory].append(event)
+	return json.dumps(events)
